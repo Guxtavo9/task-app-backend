7f2c62175991b16885a576e0aa831b04cd3cdbc9
Gustavo~ <>
2024-05-24 10:01:39 -0300

    Enable dynamic port and host configuration in config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
-export const PORT = 3333
-export const HOST = 'http://localhost'
+export const PORT = process.env.PORT || 5432
+export const HOST = process.env.HOST || 'http://localhost'
 
-// export const SECRET_KEY = process.env.SECRET_KEY;
+// export const SECRET_KEY = process.env.SECRET_KEY
+
+// export const TOKEN_EXPIRES_IN = '3m' // mudar depois para 1h
+// export const DB_TOKEN_EXPIRES_DAYS = 1 
